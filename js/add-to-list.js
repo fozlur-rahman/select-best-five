6b193a56625fbe979829cf12b91e45e55de3bc3e
fozlur-rahman <fozlurrahman993@gmail.com>
2022-08-23 16:07:23 +0600

    fix the card color

--- js/add-to-list.js
+++ js/add-to-list.js
@@ -1,19 +1,21 @@
 const players = [];
 function displayPlayers(addPlayers) {
     const playersList = document.getElementById('players-list');
+    playersList.innerText = "";
     for (let i = 0; i < addPlayers.length; i++) {
         const name = players[i].playerName;
         const li = document.createElement('li');
         li.innerText = name;
         console.log(name)
         playersList.appendChild(li);
+        
     }
-    if (addPlayers.length >= 5) {
+    if (addPlayers.length > 5) {
         return alert('Already Five selected');
     }
 }
 // document.getElementById('add-btn-1')('click', function () {
-    
+
 // })
 function addToList(element) {
     const player = element.parentNode.children[0].innerText;
@@ -21,7 +23,9 @@ function addToList(element) {
         playerName: player,
     }
     players.push(playerDetails);
+    console.log(players);
     displayPlayers(players);
-    element.disabled = true;    
+    element.disabled = true;
+    element.style.backgroundColor = " gray";
 }
 
